879c0706f654fb8ffea698c78da3f118f254f015
val <valenvillar.neco@gmail.com>
2026-06-19 18:43:45 -0300

    cambio en el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import './styles/App.css'
+import './styles/Header.css'
+import './styles/Main.css'
+import './styles/Footer.css'
 import { Init } from './components/pages/inicio'
 import { ContactM } from './components/pages/contact'
 
