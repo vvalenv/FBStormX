eec54a606386c8958a9c849a8ec7a328eb27bbc8
val <valenvillar.neco@gmail.com>
2026-07-28 11:43:46 -0300

    cambio de correo

--- src/components/layouts/main-c.jsx
+++ src/components/layouts/main-c.jsx
@@ -4,7 +4,7 @@ export function ContactMain() {
             <h1>FB StormX</h1>
             <h2>Location: 3750 NW 87th Ave, Suite 700, Doral, FL 33178</h2>
             <h3>Phone number 305-519-1550</h3>
-            <h3>Email address: FBStormX@gmail.com</h3>
+            <h3>Email address: consultation@fbstormx.com</h3>
         </main>
     )
 }
